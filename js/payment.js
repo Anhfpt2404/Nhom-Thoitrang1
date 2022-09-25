3761cd1b512eef23c5e15d7cc9ac09bd7768b7ff
NgoMinhKhoi2603 <109575681+NgoMinhKhoi2603@users.noreply.github.com>
2022-09-25 13:18:20 +0700

    up

---
+++ js/payment.js
@@ -0,0 +1,51 @@
+const btn = document.querySelectorAll("button.bt");
+//console.log(btn);
+btn.forEach(function(bt,index){
+    bt.addEventListener("click",function (event){{
+        var btnItem = event.target
+        var product = btnItem.parentElement
+        var product2 =product.parentElement
+        var productImg = product2.querySelector("img").src
+        var productName = product2.querySelector("p.name-item").innerText
+        var productPrice = product2.querySelector("p.giatien").innerText
+        addcart(productImg,productName,productPrice);
+        }})
+    })
+var i=1;
+var dem = 0;
+var sum = 0;
+function addcart(productImg,productName,productPrice){
+    var addbuy = document.createElement("tr")
+    addbuy.innerHTML = `<tr><td class="td-buy"><img style="width: 50px" src="${productImg}"/></p></td><td class="td-buy"><span>${productName}</span></td><td class="td-buy"><span>${productPrice}</span><sup>đ</sup></td><td class="td-buy"><input type="radio" name="size${i}" value="s" required/><span style="font-size: 17px">S <span><input type="radio" name="size${i}" value="m" required/><span style="font-size: 17px">M <span><input type="radio" name="size${i}" value="l" required/><span style="font-size: 17px">L <span></td></tr>`
+    i++;
+    var cart = document.querySelector("tbody")
+    cart.append(addbuy);
+    const x = parseInt(productPrice, 10);
+    sum+=x;
+    var sp = document.getElementById("sumprice");
+    sp.innerText = new Intl.NumberFormat('it-IT').format(sum);
+    dem++;
+    var sl = document.getElementById("sldh")
+    sl.innerText = dem;
+
+}
+
+var x = `<section class="section main2">
+    <div class="cl-left">
+        <h1 class="info">Thông tin giao hàng</h1>
+        <form method="get" action="#">
+            <input class="ip" type="text" name="name" placeholder="Họ và tên" required/>
+            <input class="ip" type="email" name="email" placeholder="Email" required/>
+            <input class="ip" type="tel" name="telephone" placeholder="Số điện thoại" required/>
+            <input class="ip" type="text" name="address" placeholder="Địa chỉ" required/>
+            <input class="ip" type="text" name="note" placeholder="Ghi chú"/>
+            <button type="submit" name="sm">Hoàn tất đơn hàng</button>
+        </form>
+    </div>
+    <div class="cl-rignt">
+        <div class="logo-payment"><img src="images/logoeproject.png"/> </div>
+    </div>
+</section>`;
+function warning() {
+    document.getElementById("warning").innerHTML = x;
+}
